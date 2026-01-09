37edc1aef76d83693c623614674be5c6e2472b08
Sahil-Singh23 <sahil091205@gmail.com>
2026-01-09 14:51:50 +0530

    first commit single room logic

---
+++ ChatAppBE/src/index.ts
@@ -0,0 +1,19 @@
+
+import { WebSocketServer,WebSocket } from "ws";
+
+const wss = new WebSocketServer({port:8000});
+
+const users:WebSocket[] = [];
+
+wss.on("connection",(socket)=>{
+    console.log("User connected ");
+    users.push(socket);
+    //console.log(users);
+    socket.on("message",(e)=>{
+        for(let i=0 ; i<users.length ; i++){
+            if(users[i]!==socket){
+                users[i]!.send(e.toString());
+            }
+        }
+    })
+})
