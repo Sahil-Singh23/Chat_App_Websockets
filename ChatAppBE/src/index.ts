06654bd2d41cf9f21d6a92546863ed3b35c51b2d
Sahil-Singh23 <sahil091205@gmail.com>
2026-01-13 00:59:31 +0530

    Fix: Convert PORT to number

--- ChatAppBE/src/index.ts
+++ ChatAppBE/src/index.ts
@@ -5,7 +5,7 @@ import random from "./utils.js";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+const PORT = Number(process.env.PORT) || 8000;
 
 const app = express();
 app.use(express.json());
