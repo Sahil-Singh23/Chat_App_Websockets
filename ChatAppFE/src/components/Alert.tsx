64020e3c182afd6ef19c73d38429f758e4e83f32
Sahil-Singh23 <sahil091205@gmail.com>
2026-01-28 09:24:43 +0530

    changed z on alerts to come on top

--- ChatAppFE/src/components/Alert.tsx
+++ ChatAppFE/src/components/Alert.tsx
@@ -19,7 +19,7 @@ const Alert = ({ message, type = 'success', onClose, duration = 2500 }: AlertPro
   const bgColor = type === 'success' ? 'bg-[#fff]' : type === 'error' ? 'bg-red-400' : 'bg-white-700'
 
   return (
-    <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 animate-slide-down">
+    <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-60 animate-slide-down">
       <div className={`${bgColor} text-[#080605] px-6 py-3 rounded-xl shadow-lg flex items-center gap-3`}>
         <span className="font-sfmono text-sm">{message}</span>
         <button 
